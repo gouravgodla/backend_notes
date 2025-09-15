3aa321879335fd9ac123d10c9bd84929b648e6fb
gouravgodla <98866998+gouravgodla@users.noreply.github.com>
2025-09-15 22:46:33 +0530

    open source started

--- 04_backend_main/src/controllers/user.controller.js
+++ 04_backend_main/src/controllers/user.controller.js
@@ -150,11 +150,13 @@ const logoutUser = asyncHandler(async (req, res) => {
     await User.findByIdAndUpdate(
         req.user._id,
         {
-            $set: {
-                refreshToken: undefined
+            $unset: {
+                refreshToken: 1
             }
         },
-        { new: true }
+        {
+            new: true
+        }
     )
     const options = {
         httpOnly: true,
